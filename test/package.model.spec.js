346e14733c05a0dc19a9b62e16b1b3b20681be6d
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2017-11-16 20:56:11 -0500

    update

--- test/package.model.spec.js
+++ test/package.model.spec.js
@@ -47,6 +47,7 @@ describe('Page model', function () {
     });
 
     describe('Class methods', function () {
+        /*
         beforeEach(function (done) {
             Page.create({
                 title: 'foo',
@@ -59,25 +60,39 @@ describe('Page model', function () {
                 .catch(done);
 
         });
+        */
         describe('findByTag', function () {
+            beforeEach(function(){
+                return Promise.all([
+                    Page.create({
+                        title: 'Test Page 1',
+                        content: 'this is content 1',
+                        tags:['misc', 'test', 'star-war']
+                    }),
+                    Page.create({
+                        title: 'Test Page 2',
+                        content: 'Other stuff',
+                        tags:['misc', 'star-wars', 'fsa']
+                    })
+                ]);
+            });
            
             it('gets pages with the search tag', function (done) {
-                Page.findByTag('bar')
+                Page.findByTag('test')
                     .then(function (pages) {
                        // console.log("here is pages: ",pages)
                         expect(pages).to.have.lengthOf(1);
+                        expect(pages[0].title).to.equal('test page 1');
                         done();
                     })
                     .catch(done);
             });
 
             it('does not get pages without the search tag', function (done) {
-                Page.findByTag('falafel')
-                    .then(function (pages) {
-                        expect(pages).to.have.lengthOf(0);
-                        done();
-                    })
-                    .catch(done);
+                return Page.findByTag('foobar')
+                           .then(function(pages){
+                            expect(pages).to.have.lengthOf(0);
+                           });
             });
         });
 
@@ -118,22 +133,126 @@ describe('Page model', function () {
 
 
 
-//  describe('Instance methods', function () {
-//    describe('findSimilar', function () {
-//      it('never gets itself');
-//      it('gets other pages with any common tags');
-//      it('does not get other pages without any common tags');
-//    });
-//  });
+ describe('Instance methods', function () {
+   
+   describe('findSimilar', function () {
 
-//  describe('Validations', function () {
-//    it('errors without title');
-//    it('errors without content');
-//    it('errors given an invalid status');
-//  });
+    var rosePage, berryPage, bananaPage;
+    beforeEach(function(){
+        return Promise.all([
+            Page.create({
+                title: 'Rose',
+                content:'pretty flowers, watch out for thorns',
+                tags:['flower', 'red']
+            }),
+            Page.create({
+                title: 'Strawberry',
+                content:'Edible things, dip in chocolate',
+                tags:['red','fruit']
+            }),
+            Page.create({
+                title:'Banana',
+                content:'Edible things, dip in penut butter',
+                tags:['fruit', 'yellow']
+            })
 
-//  describe('Hooks', function () {
-//    it('it sets urlTitle based on title before validating');
-//  });
+        ])
+         .spread(function(_p1, _p2, _p3){
+            rosePage=_p1;
+            berryPage=_p2;
+            bananaPage=_p3;
+         });
+    });
+
+     it('never gets itself', function(){
+        berryPage.findSimilar()
+            .then(function(similar){
+                expect(similar).to.not.contain.a.thing.with.property('id', berryPage.id);
+            })
+     });
+
+
+
+
+     it('gets other pages with any common tags', function(){
+        return berryPage.findSimilar()
+                        .then(function(similar){
+                            expect(similar).to.have.lengthOf(2);
+                            expect(similar).to.contain.a.thing.with.property('id', rosePage.id);
+                            expect(similar).to.contain.a.thing.with.property('id', bananaPage.id);
+                        });
+     });
+
+
+     it('does not get other pages without any common tags', function(){
+        return bananaPage.findSimilar()
+                         .then(function(similar){
+                            expect(similar).to.have.lengthOf(1);
+                            expect(similar).to.contain.a.thing.with.property('id', berryPage.id);
+                            
+                        });
+     });
+   });
+ });
+
+
+ describe('Validations', function () {
+   it('errors without title',function(){
+    var page=Page.build({});
+    return page
+            .validate()
+            .catch(function(err){
+                expect(err).to.exist;
+                expect(err.errors).to.contain.a.thing.with.property('path','title');
+            });
+   });
+
+   it('errors without content',function(){
+    var page=Page.build({});
+    return page
+           .validate()
+           .catch(function(err){
+            expect(err).to.exist;
+            expect(err.errors).to.contain.a.thing.with.property('path','content');
+           });
+   });
+   it('errors given an invalid status',function(){
+    var page=Page.build({
+        title:'kjkja',
+        content:'askjka',
+        status:'evil'
+    })
+     return page
+               .save()
+               .then(function(){
+                throw Error('Promise should have rejected');
+               }, function(err){
+                 expect(err).to.exist;
+                 expect(err.message).to.contain('status');
+               });
+   });
+
+   it ('will be valide with the abve stuff', function(){
+    var page=Page.build({
+        title:'foobar',
+        content:'Foos and bars but together'
+    });
+     return page.save();
+   });
+ });
+
+ describe('Hooks', function () {
+   it('it sets urlTitle based on title before validating', function(){
+
+       var page=Page.build({
+           title:'the who',
+           content:'a band on first base'
+       });
+       return page.save()
+                  .then(function(){
+                    expect(page.urlTitle).to.equal('the who');
+                  })
+     });
+    });
 
  });
